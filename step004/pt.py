9b074c6e8e05cac9915e2d79c381e8c607c725fa
ryanrasoarahona3 <rasoarahonarivoniaina@ku.th>
2022-07-13 18:05:10 +0300

    Initial commit — to be removed

---
+++ step004/pt.py
@@ -0,0 +1,101 @@
+
+# region imports
+from AlgorithmImports import *
+from collections import deque
+import numpy as np
+import itertools as it
+from math import floor
+# endregion
+
+class CrawlingVioletFalcon(QCAlgorithm):
+
+    def Initialize(self):
+        self.SetStartDate(2014, 1, 1)
+        self.SetEndDate(2018, 1, 1)
+        self.SetCash(100000)
+
+        tickers = ['XLK', 'QQQ', 'BANC', 'BBVA', 'BBD', 'BCH', 'BLX', 'BSBR', 'BSAC', 'SAN',
+                   'CIB', 'BXS', 'BAC', 'BOH', 'BMO', 'BK', 'BNS', 'BKU', 'BBT', 'NBHC', 'OFG',
+                   'BFR', 'CM', 'COF', 'C', 'VLY', 'WFC', 'WAL', 'WBK', 'RBS', 'SHG', 'STT', 'STL', 'SCNB', 'SMFG',
+                   'STI']
+        self.threshold = 2
+        self.symbols = []
+        
+        for i in tickers:
+            self.symbols.append(
+                self.AddEquity(i, Resolution.Daily).Symbol
+            )
+        
+        self.pairs = {}
+        self.formation_period = 252
+
+        self.history_price = {}
+        for symbol in self.symbols:
+            hist = self.History([symbol], self.formation_period+1, Resolution.Daily)
+            if hist.empty:
+                self.symbols.remove(symbol)
+            else:
+                self.history_price[str(symbol)] = deque(maxlen=self.formation_period)
+                for tuple in hist.loc[str(symbol)].itertuples():
+                    self.history_price[str(symbol)].append(float(tuple.close))
+                if len(self.history_price[str(symbol)]) < self.formation_period:
+                    self.symbols.remove(symbol)
+                    self.history_price.pop(str(symbol))
+        
+        self.symbol_pairs = list(it.combinations(self.symbols, 2))
+        self.AddEquity("SPY", Resolution.Daily)
+        self.Schedule.On(self.DateRules.MonthStart("SPY"), self.TimeRules.AfterMarketOpen("SPY"), self.Rebalance)
+        self.count = 0
+        self.sorted_pairs = None
+
+    def OnData(self, data: Slice):
+        for symbol in self.symbols:
+            if data.Bars.ContainsKey(symbol) and str(symbol) in self.history_price:
+                self.history_price[str(symbol)].append(float(data[symbol].Close))
+        if self.sorted_pairs is None:
+            return
+        
+        for i in self.sorted_pairs:
+            spread = np.array(self.history_price[str(i[0])]) - np.array(self.history_price[str(i[1])])
+            mean = np.mean(spread)
+            std = np.std(spread)
+            ratio = self.Portfolio[i[0]].Price / self.Portfolio[i[1]].Price
+
+            if spread[-1] > mean + self.threshold * std:
+                if not self.Portfolio[i[0]].Invested and not self.Portfolio[i[1]].Invested:
+                    quantity = int(self.CalculateOrderQuantity(i[0], 0.2))
+                    self.Sell(i[0], quantity)
+                    self.Buy(i[1], floor(quantity * ratio))
+                
+            elif spread[-1] < mean - self.threshold * std:
+                if not self.Portfolio[i[0]].Invested and not self.Portfolio[i[1]].Invested:
+                    quantity = int(self.CalculateOrderQuantity(i[0], 0.2))
+                    self.Sell(i[1], quantity)
+                    self.Buy(i[0], floor(quantity * ratio))
+            
+            elif self.Portfolio[i[0]].Invested and self.Portfolio[i[1]].Invested:
+                self.Liquidate(i[0])
+                self.Liquidate(i[1])
+
+    def Rebalance(self):
+        if self.count % 6 == 0:
+            distances = {}
+            for i in self.symbol_pairs:
+                distances[i] = Pair(i[0], i[1],
+                    self.history_price[str(i[0])],
+                    self.history_price[str(i[1])]
+                ).distance()
+                self.sorted_pairs = sorted(distances, key=lambda x: distances[x])[:4]
+        self.count+= 1
+
+class Pair:
+    def __init__(self, symbol_a, symbol_b, price_a, price_b):
+        self.symbol_a = symbol_a
+        self.symbol_b = symbol_b
+        self.price_a = price_a
+        self.price_b = price_b
+    
+    def distance(self):
+        norm_a = np.array(self.price_a) / self.price_a[0]
+        norm_b = np.array(self.price_b) / self.price_b[0]
+        return sum((norm_a - norm_b) ** 2)
---
+++ step2/main.py
@@ -0,0 +1,63 @@
+# region imports
+from AlgorithmImports import *
+# endregion
+
+class HyperActiveFluorescentOrangeDog(QCAlgorithm):
+
+    def Initialize(self):
+        self.SetStartDate(2017, 1, 1)
+        self.SetEndDate(2017, 4, 1)
+        self.SetCash(100000)
+
+        equity = self.AddEquity("IBM", Resolution.Minute)
+        equity.SetDataNormalizationMode(DataNormalizationMode.Raw)
+        self.underlying = equity.Symbol
+        self.SetBenchmark(self.underlying)
+
+        self.call = str()
+
+
+    def OnData(self, slice):
+        #if not self.Portfolio.Invested:
+        #    self.SetHoldings("SPY", 0.33)
+        #    self.SetHoldings("BND", 0.33)
+        #    self.SetHoldings("AAPL", 0.33)
+        if not self.Portfolio[self.underlying].Invested:
+            self.MarketOrder(self.underlying, 100)
+        if not (self.Securities.ContainsKey(self.call) and self.Portfolio[self.underlying].Invested):
+            self.call = self.AddContract(slice)
+        if self.Securities.ContainsKey(self.call) and not self.Portfolio[self.call].Invested:
+            self.Sell(self.call, 1)
+    
+    def AddContract(self, slice):
+        filtered_contracts = self.InitialFilter(-3, 3, 0, 30)
+        if len(filtered_contracts) == 0: return str()
+        else:
+            call = [x for x in filtered_contracts if x.ID.OptionRight == OptionRight.Call]
+            contracts = sorted(sorted(call, key=lambda x: abs(self.Securities[self.underlying].Price - x.ID.StrikePrice)),
+                key=lambda x: x.ID.Date, reverse=True
+            )
+            if len(contracts) > 0:
+                self.AddOptionContract(contracts[0], Resolution.Minute)
+                return contracts[0]
+            else:
+                return str()
+    
+    def InitialFilter(self, min_strike_rank, max_strike_rank, min_expiry_date, max_expiry_date):
+        contracts = self.OptionChainProvider.GetOptionContractList(self.underlying, self.Time.date())
+        if len(contracts) == 0: return []
+        contract_list = [i for i in contracts if min_expiry_date < (i.ID.Date.date() - self.Time.date()).days < max_expiry_date]
+        atm_strike = sorted(contract_list, key=lambda x:abs(x.ID.StrikePrice - self.Securities[self.underlying].Price))[0].ID.StrikePrice
+        strike_list = sorted(set([i.ID.StrikePrice for i in contract_list]))
+        atm_strike_rank = strike_list.index(atm_strike)
+        try:
+            strikes = strike_list[(atm_strike_rank+min_strike_rank):(atm_strike_rank+max_strike_rank)]
+        except:
+            strikes = strike_list
+        filtered_contracts = [i for i in contract_list if i.ID.StrikePrice in strikes]
+        return filtered_contracts
+    
+    def OnOrderEvent(self, orderEvent):
+        self.Log(str(orderEvent))
+
+
